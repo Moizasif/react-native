9c207ddf2e911b8d1b2176a768c3d565a4e9eefa
Moizasif <kmoizasif019@gmail.com>
2021-03-27 15:44:34 +0500

    Working with states and events

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal] );
+  }
   return (
     <View style={styles.mainview}>
       <View style={styles.inputContainer}>
-           <TextInput placeholder="Course Goal" style={styles.inputBox}/>
-           <Button title="ADD"  />
+           <TextInput 
+           placeholder="Course Goal"
+           style={styles.inputBox}
+           onChangeText={goalInputHandler}
+           value={enteredGoal}/>
+           <Button title="ADD"  onPress={addGoalHandler}/>
+      </View>
+      <View>
+
       </View>
     </View>
   );
