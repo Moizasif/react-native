9ddf234eeb8abddb152741b63f125b6b64b7ba28
Moizasif <kmoizasif019@gmail.com>
2021-03-27 14:40:08 +0500

    Flex Box Intro But using Inline Styles

--- App.js
+++ App.js
@@ -3,16 +3,13 @@ import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View >
-      <View style={{padding:30}}>
-           <TextInput placeholder="Add Some Value" style={{borderColor:'black', borderWidth:1, padding:6 }}/>
+    <View style={{padding:50}}>
+      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+           <TextInput 
+           placeholder="Course Goal"
+           style={{width:"80%", borderColor:'black', borderWidth:1, padding:6 }}/>
            <Button title="ADD"  />
       </View>
-      <View>
-
-      </View>
-      
-     
     </View>
   );
 }
