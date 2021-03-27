b011dd92b057efa4112fd952be620ba865e9322c
Moizasif <kmoizasif019@gmail.com>
2021-03-27 15:22:08 +0500

    Convert styles into Stylesheet Objects

--- App.js
+++ App.js
@@ -3,11 +3,9 @@ import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding:50}}>
-      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
-           <TextInput 
-           placeholder="Course Goal"
-           style={{width:"80%", borderColor:'black', borderWidth:1, padding:6 }}/>
+    <View style={styles.mainview}>
+      <View style={styles.inputContainer}>
+           <TextInput placeholder="Course Goal" style={styles.inputBox}/>
            <Button title="ADD"  />
       </View>
     </View>
@@ -15,5 +13,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
- 
+ mainview: {
+   padding: 50
+ },
+ inputContainer: {
+  flexDirection:'row',
+   justifyContent:'space-between',
+    alignItems:'center'
+ },
+ inputBox: {
+  width:"80%",
+   borderColor:'black',
+    borderWidth:1,
+     padding:6
+ }
 });
