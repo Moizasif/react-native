1985ea9e96e36578db8e3e15ce7536530721597f
Moizasif <kmoizasif019@gmail.com>
2021-03-28 01:50:10 +0500

    Working with Touchable Components

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const GoalItem = (props) => {
     return (
-        <View style={styles.listItem}>
+        <TouchableOpacity activeOpacity={0.5} onPress={props.onDelete}>
+             <View style={styles.listItem}>
                 <Text>{props.title}</Text>
-       </View>
+             </View>
+        </TouchableOpacity>
+       
     )
 }
 
