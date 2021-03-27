5840e2748f0bf66978a3b9fef21be816809b99c6
Moizasif <kmoizasif019@gmail.com>
2021-03-27 14:25:40 +0500

    Getting starrted with styles

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <View style={{padding:30}}>
+           <TextInput placeholder="Add Some Value" style={{borderColor:'black', borderWidth:1, padding:6 }}/>
+           <Button title="ADD"  />
+      </View>
+      <View>
+
+      </View>
+      
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
