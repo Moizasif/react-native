33deb2b44231051ac2500e592fb5acbe35e3d776
Moizasif <kmoizasif019@gmail.com>
2021-03-28 15:29:19 +0500

    Adding a Modal Overlay

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,36 +1,38 @@
-import React, {useState} from 'react';
-import {View, TextInput, Button, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 
 const GoalInput = props => {
     const [enteredGoal, setEnteredGoal] = useState('');
 
     const goalInputHandler = (enteredText) => {
         setEnteredGoal(enteredText);
-      }
+    }
     return (
-    <View style={styles.inputContainer}>
-        <TextInput 
-        placeholder="Course Goal"
-        style={styles.inputBox}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}/>
-        <Button title="ADD"  onPress={props.onAddGoal.bind(this, enteredGoal)}/>
-   </View>
+        <Modal visible={props.visible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Course Goal"
+                    style={styles.inputBox}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal} />
+                <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+            </View>
+        </Modal>
     )
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection:'row',
-         justifyContent:'space-between',
-          alignItems:'center'
-       },
-       inputBox: {
-        width:"80%",
-         borderColor:'black',
-          borderWidth:1,
-           padding:6
-       }
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    inputBox: {
+        width: "80%",
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 6
+    }
 })
 
 export default GoalInput
